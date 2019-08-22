220c25f2a4d28175a2ba98fe69d6ed12e7acccce
autumn-ragland <raglanda998@gmail.com>
2019-08-22 14:35:05 -0500

    Graded 08-22-2019 14:34:25

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # python-basics1-review-cw
-
+1/3 + partial credit on #3.See grades.py for comments. Score:2
 ### Problem 1:
 Create a program that prints the user input until they enter 'q' to quit.
 
--- graded.py
+++ graded.py
@@ -1,10 +1,10 @@
 # Problem 1:
 # Create a program that prints the user input until they enter 'q' to quit.
 
-# userInput = input("Text Goes Here")
-# while userInput != 'q':
-#     userInput = input("Text Goes Here")
-#     print(userInput)
+userInput = input("Text Goes Here")
+while userInput != 'q':
+    userInput = input("Text Goes Here")
+    print(userInput)
 
 
 
@@ -14,26 +14,25 @@
 # If they enter 3 print 3.
 # If they enter ‘q’ or 0 (your choice), quit. Else, print “ERROR”.
 
-# userInput = input("Enter the number 1 , 2, or 3")
-# while userInput != 'q':
-#     userInput = int(input("Enter the number 1, 2, or 3"))
-#     if userInput == 1:
-#         print(userInput)
-#     elif userInput == 2:
-#         print(userInput)
-#     elif userInput == 3:
-#         print(userInput)
-#     elif userInput == 'q':
-#         print("Good-bye")
-#     else:
-#         print("Error")
+userInput = input("Enter the number 1 , 2, or 3")
+while userInput != 'q':
+    userInput = int(input("Enter the number 1, 2, or 3")) # you can't compare a string to an integer
+    if userInput == 1:
+        print(userInput)
+    elif userInput == 2:
+        print(userInput)
+    elif userInput == 3:
+        print(userInput)
+    elif userInput == 'q':
+        print("Good-bye")
+    else:
+        print("Error")
 
 # Problem 4:
 # Create a program that takes the user input until they enter 'q'.
 # You should store all of their input and separate the input with a comma.
 # Once they enter 'q', print all of the comma separated words they entered.
 
-userInput = input("Enter Input here")
-while userInput != "q":
-    
-
+# userInput = input("Enter Input here")
+# while userInput != "q":
+ 
