09fc9480c009964974abfa168a33a891233417fd
flashfan88 <lavarjohnson20@gmail.com>
2019-08-22 12:20:11 -0500

    almost

---
+++ graded.py
@@ -0,0 +1,39 @@
+# Problem 1:
+# Create a program that prints the user input until they enter 'q' to quit.
+
+# userInput = input("Text Goes Here")
+# while userInput != 'q':
+#     userInput = input("Text Goes Here")
+#     print(userInput)
+
+
+
+# Problem 3:
+# Create a program that takes user input in a while loop.
+# If they enter 1, print 1. If they enter 2, print 2.
+# If they enter 3 print 3.
+# If they enter ‘q’ or 0 (your choice), quit. Else, print “ERROR”.
+
+# userInput = input("Enter the number 1 , 2, or 3")
+# while userInput != 'q':
+#     userInput = int(input("Enter the number 1, 2, or 3"))
+#     if userInput == 1:
+#         print(userInput)
+#     elif userInput == 2:
+#         print(userInput)
+#     elif userInput == 3:
+#         print(userInput)
+#     elif userInput == 'q':
+#         print("Good-bye")
+#     else:
+#         print("Error")
+
+# Problem 4:
+# Create a program that takes the user input until they enter 'q'.
+# You should store all of their input and separate the input with a comma.
+# Once they enter 'q', print all of the comma separated words they entered.
+
+userInput = input("Enter Input here")
+while userInput != "q":
+    
+
